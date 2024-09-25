c339a6a2fe22b84060269c2e9ff2f3b54b5bac1e
quanglinh <116327425+Linh1023@users.noreply.github.com>
2024-09-25 15:47:39 +0700

    fix regex tour update request

--- src/main/java/com/tour/tour_management/dto/request/tour/TourUpdateRequest.java
+++ src/main/java/com/tour/tour_management/dto/request/tour/TourUpdateRequest.java
@@ -17,7 +17,7 @@ import jakarta.validation.constraints.NotBlank;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TourUpdateRequest {
 
-    @Pattern(regexp = "^(?=(.*\\p{L}){2,})[\\p{L} ]{2,255}",message ="TOUR_NAME_INVALID" )
+    @Pattern(regexp = "^(?=(.*\\p{L}){2,})[\\p{L}\\p{N} ]{2,255}$",message ="TOUR_NAME_INVALID" )
     @NotBlank(message = "TOUR_NAME_NOT_BLANK")
     String tour_name;
 
